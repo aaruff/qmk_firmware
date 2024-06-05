da80d22fe30471495d10d12efd617629b8b73bb0
Anwar A. Ruff <anwarruff@gmail.com>
2024-06-04 22:01:13 -0400

    move plus and minus key

--- keyboards/sofle_choc/keymaps/aruff_sofle_choc/keymap.c
+++ keyboards/sofle_choc/keymaps/aruff_sofle_choc/keymap.c
@@ -24,10 +24,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
+ * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
  * |  ~   |   !  |   @  |   #  |   $  |   %  |                    |   ^  |   &  |   *  |   (  |   )  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | TAB  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |      |
+ * | TAB  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  =   |
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | ESC  |   A  |   S  |   D  |   F  |   G  |                    |   H  |   J  |   K  |   L  |   ;  | BSC  |
@@ -40,8 +40,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY]=LAYOUT(
-     KC_GRV,  KC_1,   KC_2,    KC_3,   KC_4,   KC_5,                        KC_6,  KC_7,      KC_8,    KC_9,     KC_0, XXXXXXX,
-     KC_TAB,  KC_Q,   KC_W,    KC_E,   KC_R,   KC_T,                        KC_Y,  KC_U,      KC_I,    KC_O,     KC_P,  XXXXXXX,
+     KC_GRV,  KC_1,   KC_2,    KC_3,   KC_4,   KC_5,                        KC_6,  KC_7,      KC_8,    KC_9,     KC_0, KC_MINS,
+     KC_TAB,  KC_Q,   KC_W,    KC_E,   KC_R,   KC_T,                        KC_Y,  KC_U,      KC_I,    KC_O,     KC_P,  KC_EQL,
     KC_ESC,  KC_A,   SMB_S,    KC_D,   NAV_F,   KC_G,                        KC_H,  KC_J,      KC_K,    KC_L,  KC_SCLN,  KC_BSPC,
     KC_LSFT, KC_Z,  KC_X,   KC_C,  KC_V,   KC_B, XXXXXXX,      XXXXXXX, KC_N, KC_M,  KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
             KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SMB,                        KC_FKEY, KC_ENT, KC_RGUI, KC_RALT, KC_RCTL
