023d3a36e71fabd5a531d0c8480dd0d213ff558d
Anwar A. Ruff <anwarruff@gmail.com>
2024-10-20 17:52:57 -0400

    Replace left most thumb cluster key for the right hand with backspace

--- keyboards/sofle_choc/keymaps/aruff_sofle_choc/keymap.c
+++ keyboards/sofle_choc/keymaps/aruff_sofle_choc/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------'    '-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------|-------.    .-------|-----------------------------------------'
- *               | CTRL | ALT  | LGUI | SPC  | SYMB  |    | SYMB  | ENT  | RGUI | ALT  | CTRL  |
+ *               | CTRL | ALT  | LGUI | SPC  | SYMB  |    | BKSP  | ENT  | RGUI | ALT  | CTRL  |
  *               `-----------------------------------'    '------------------------------------'
  */
 
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB,  KC_Q,   KC_W,    KC_E,   KC_R,   KC_T,                        KC_Y,   KC_U,       KC_I,    KC_O,      KC_P,  KC_EQL,
     KC_ESC,  KC_A,   KC_S,    KC_D,   NAV_F,   KC_G,                        KC_H,   KC_J,       KC_K,    KC_L, FKEY_SCLN,  KC_QUOT,
     KC_LSFT, KC_Z,  KC_X,   KC_C,  KC_V,   KC_B, XXXXXXX,          XXXXXXX, KC_N,   KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, KC_RSFT,
-            KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SMB,                    KC_SMB, KC_ENT,    KC_RGUI, KC_RALT,   KC_RCTL
+            KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SMB,                    KC_BSPC, KC_ENT,    KC_RGUI, KC_RALT,   KC_RCTL
 ),
 
 /* SYMBL
